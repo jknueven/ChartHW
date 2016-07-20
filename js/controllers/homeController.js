c4cc921cef4585874c0aabbf38bf2bff482197bc
Jared <jcknueven@gmail.com>
2016-07-20 15:03:42 -0400

    added pie chart

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -10,44 +10,84 @@ angular.module("mtg").controller("homeController", function(API) {
 	
 	var cardData = API.getCards();
 
-	cardData.then(function(cards){
+	cardData.then(function(response){
 	 	
-	 	cardData = cards.data.cards;
+	 	cardData = response.data.cards;
+	 	
+		var colorArray = [];
+	 	var White = 0;
+		var Blue = 0;
+		var Black = 0;
+		var Red = 0;
+		var Green = 0;
 
-	 	 var colorMap = cardData.map(function(cardData){
-	 	 	console.log(cardData.color);
-          return cardData.color;
-        });
+	 	 var colorMap = cardData.map(function(cards){
+	 	 	console.log(cards);
 
-        var cmcMap = cardData.map(function(cardData){
-          return cardData.cmc;
-        });
+	 	 	if(cards.colors){
+		 	 	cards.colors.forEach(function(color){
+		 	 		console.log(color);
+		 	 		
 
-	 });
-
-	var sets = API.getSets();
-
-	sets.then(function(sets){
-
-	});
-
-	var myChart = new Chart(ctx, {
-            type: 'bar',
+		 	 	if (color === 'White')
+		 	 	{
+		 	 		White ++;
+		 	 	}
+		 	 	else if (color === 'Blue')
+		 	 	{
+		 	 		Blue ++;
+		 	 	}
+		 	 	else if (color === 'Black')
+		 	 	{
+		 	 		Black ++;
+		 	 	}
+		 	 	else if (color === 'Red')
+		 	 	{
+		 	 		Red ++;
+		 	 	}
+		 	 	else if (color === 'Green')
+		 	 	{
+		 	 		Green ++;
+		 	 	}
+		 	 	});
+	 	 	};
+	 	 	});
+	 	 	var colorArray = [White,Blue,Black,Red,Green];
+	 	 	var myChart = new Chart(ctx, {
+            type: 'pie',
             data: {
                 labels: ["White", "Blue", "Black", "Red", "Green"],
                 datasets: [{
                     label: 'Maxs',
-                    data: colorMap,
-                    backgroundColor: 'rgba(255,0,0,.2)',
+                    data: colorArray,
+                    backgroundColor: ['rgb(255, 252, 211)',
+                    'rgb(169, 220, 239)',
+                    'rgb(197, 190, 184)',
+                    'rgb(238, 164, 137)',
+                    'rgb(147, 203, 164)',],
+                    borderColor: '#886827',
                 },
-                {
-                    label: 'Mins',
-                    data: cmcMap,
-                    backgroundColor: 'rgba(0,0,255,.4)',
-                }
-                ]
+                ],
             },
+            options: {
+        		tooltips: {
+           		callbacks: {
+                label: function(tooltipItem, data) {
+                    var allData = data.datasets[tooltipItem.datasetIndex].data;
+                    var tooltipLabel = data.labels[tooltipItem.index];
+                    var tooltipData = allData[tooltipItem.index];
+                    var total = 0;
+                    for (var i in allData) {
+                        total += allData[i];
+                    }
+                    var tooltipPercentage = Math.round((tooltipData / total) * 100);
+                    return tooltipLabel + ': ' + tooltipData + ' (' + tooltipPercentage + '%)';
+                	}
+            	}
+        	  }
+    		}
         });
-
-
+	 });
 });
+
+	
