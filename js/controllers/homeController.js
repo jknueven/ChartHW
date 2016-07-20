138ad30d7670b85bff64e511d0b27ef97b2d9fa2
Jared <jcknueven@gmail.com>
2016-07-20 13:51:02 -0400

    working on chart data

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -3,11 +3,25 @@
 angular.module("mtg").controller("homeController", function(API) {
 
 	var vm = this;
-	
-	var data = API.getCards();
 
-	data.then(function(cards){
+	var ctx = document.getElementById("myChart");
+    var ctx = document.getElementById("myChart").getContext("2d");
+
+	
+	var cardData = API.getCards();
+
+	cardData.then(function(cards){
 	 	
+	 	cardData = cards.data.cards;
+
+	 	 var colorMap = cardData.map(function(cardData){
+	 	 	console.log(cardData.color);
+          return cardData.color;
+        });
+
+        var cmcMap = cardData.map(function(cardData){
+          return cardData.cmc;
+        });
 
 	 });
 
@@ -15,7 +29,25 @@ angular.module("mtg").controller("homeController", function(API) {
 
 	sets.then(function(sets){
 
-	}):
+	});
+
+	var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ["White", "Blue", "Black", "Red", "Green"],
+                datasets: [{
+                    label: 'Maxs',
+                    data: colorMap,
+                    backgroundColor: 'rgba(255,0,0,.2)',
+                },
+                {
+                    label: 'Mins',
+                    data: cmcMap,
+                    backgroundColor: 'rgba(0,0,255,.4)',
+                }
+                ]
+            },
+        });
 
 
 });
